21c2d8e869ba576db90fdb50f5935480ea26520f
DmitriiTMS <dmitrii12_88@mail.ru>
2024-08-06 21:57:28 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,46 +1,77 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
-};
-
-function decode(expr) {
-    // write your solution here
-}
-
-module.exports = {
-    decode
-}
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    ".----": "1",
+    "..---": "2",
+    "...--": "3",
+    "....-": "4",
+    ".....": "5",
+    "-....": "6",
+    "--...": "7",
+    "---..": "8",
+    "----.": "9",
+    "-----": "0",
+  };
+  
+  function decode(expr) {
+    const newArrExpr = [];
+    const num = 10;
+  
+    for (let i = 0; i < expr.length; i += num) {
+      newArrExpr.push(expr.slice(i, i + num));
+    }
+  
+    const newArr = newArrExpr.map((elem) => {
+      return Number(elem) || elem;
+    });
+  
+    const arrResult = newArr.map((elem) => {
+        const arrElem = [];
+        const itemElem = String(elem);
+        if (itemElem.includes("*")) {
+          arrElem.push(" ");
+        } else {
+          for (let i = 0; i < itemElem.length; i += 2) {
+            if (Number(`${itemElem[i]}${itemElem[i + 1]}`) === num) {
+              arrElem.push(".");
+            } else {
+              arrElem.push("-");
+            }
+          }
+        }
+        return arrElem.join("");
+      }).map((elem) => {
+        return MORSE_TABLE[elem] || " ";
+      }).join("");
+  
+    return arrResult;
+  
+  }
+  
+  module.exports = {
+    decode,
+  };
